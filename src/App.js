c8353b9f55a28d98782687c4779c5dbe575cba7b
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2021-01-02 14:36:36 -0600

    hook setEffect for ciclo de vida

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
    // Declara una nueva variable de estado, que llamaremos "count".
@@ -7,15 +7,23 @@ function App() {
    const [libros] = useState([{ titulo: 'La vuelta al mundo en 80 dias'}, {titulo: '100 años de soledad'}])
    const [titulo, setTitulo] = useState('');
 
+
+   //componentDidMount y componentDidUpdate
+   useEffect(() => {
+     document.title = `You clicked ${count} times`;
+   });
+
    return (
      <div>
-       <h2>{titulo}</h2>
+       <h2>Hook de estado{titulo}</h2>
        <p>You clicked {count} times</p>
        <button onClick={() => setCount(count + 1)}>
          Click me
        </button>
        <p>{fruit} {libros.map((libro) => <span>{libro.titulo}</span>)}</p>
        <input type="text" onChange={e => setTitulo(e.target.value)}/>
+
+       
      </div>
    );
 }
