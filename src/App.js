dabf01109ea5cd81e8d6ed073da758a878f9e7da
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2021-01-01 22:34:48 -0600

    primer hook, estados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   // Declara una nueva variable de estado, que llamaremos "count".
+   const [count, setCount] = useState(0);
+
+   return (
+     <div>
+       <p>You clicked {count} times</p>
+       <button onClick={() => setCount(count + 1)}>
+         Click me
+       </button>
+     </div>
+   );
 }
 
 export default App;
