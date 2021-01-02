9c3fe1b12a23bf42527890e9e19f6e6f680f7fab
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2021-01-02 13:19:20 -0600

    handle function change state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import React, { useState } from 'react';
 function App() {
    // Declara una nueva variable de estado, que llamaremos "count".
    const [count, setCount] = useState(0);
+   const [fruit] = useState('manzana');
+   const [libros] = useState([{ titulo: 'La vuelta al mundo en 80 dias'}, {titulo: '100 años de soledad'}])
+   const [titulo, setTitulo] = useState('');
 
    return (
      <div>
+       <h2>{titulo}</h2>
        <p>You clicked {count} times</p>
        <button onClick={() => setCount(count + 1)}>
          Click me
        </button>
+       <p>{fruit} {libros.map((libro) => <span>{libro.titulo}</span>)}</p>
+       <input type="text" onChange={e => setTitulo(e.target.value)}/>
      </div>
    );
 }
